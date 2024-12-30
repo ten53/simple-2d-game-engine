360658a41d652eeeba78fce83c73650116520dfc
Kenneth Wright <kenneth.wright@ten53.net>
2024-12-30 05:03:09 +0100

    Added SDL object rendering and game time step

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 
@@ -73,13 +75,31 @@ void Game::ProcessInput() {
 }
 
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+
 void Game::Setup() {
-    //TODO:
+    playerPosition = glm::vec2(10.0, 50.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 
 void Game::Update() {
-    //TODO:
+    // capping frame rate
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPrevFrame);
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
+
+    // difference is ticks since last frame converted to seconds
+    double deltaTime = (SDL_GetTicks() - static_cast<double>(millisecsPrevFrame)) / 1000.0;
+
+    // save previous frame rate
+    millisecsPrevFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 
@@ -87,10 +107,18 @@ void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
     SDL_RenderClear(renderer);
 
-    // Draw Rectangle
-    SDL_SetRenderDrawColor(renderer, 213, 21, 219, 255);
-    SDL_Rect player = { 100, 100, 200, 200 };
-    SDL_RenderFillRect(renderer, &player);
+    // render png texture
+    SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect dstRect = {
+        static_cast<int>(playerPosition.x),
+        static_cast<int>(playerPosition.y),
+        32,
+        32 };
+
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
 
     SDL_RenderPresent(renderer);
 }
@@ -98,6 +126,7 @@ void Game::Render() {
 
 void Game::Run() {
     Setup();
+
     while(isRunning) {
         ProcessInput();
         Update();
--- src/Game.h
+++ src/Game.h
@@ -3,9 +3,15 @@
 
 #include <SDL2/SDL.h>
 
+
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
+
+
 class Game {
     private:
         bool isRunning;
+        int millisecsPrevFrame = 0;
         SDL_Window* window;
         SDL_Renderer* renderer;
 
